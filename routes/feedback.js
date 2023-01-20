b34fa5482667f5830bbb32f328375d23f58e82f8
donavance08 <adonisavance@gmail.com>
2023-01-21 01:57:29 +0800

    complete code

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,7 +4,14 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-module.exports = params => {
+const validations = [
+  check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
+  check('email').trim().isEmail().normalizeEmail().withMessage('A valid email address is required'),
+  check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+  check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
+];
+
+module.exports = (params) => {
   const { feedbackService } = params;
 
   router.get('/', async (request, response, next) => {
@@ -12,6 +19,9 @@ module.exports = params => {
       const feedback = await feedbackService.getList();
 
       const errors = request.session.feedback ? request.session.feedback.errors : false;
+
+      const successMessage = request.session.feedback ? request.session.feedback.message : false;
+
       request.session.feedback = {};
 
       return response.render('layout', {
@@ -19,6 +29,7 @@ module.exports = params => {
         template: 'feedback',
         feedback,
         errors,
+        successMessage,
       });
     } catch (err) {
       return next(err);
@@ -27,40 +38,46 @@ module.exports = params => {
 
   router.post(
     '/',
-    [
-      check('name')
-        .trim()
-        .isLength({ min: 3 })
-        .escape()
-        .withMessage('A name is required'),
-      check('email')
-        .trim()
-        .isEmail()
-        .normalizeEmail()
-        .withMessage('A valid email address is required'),
-      check('title')
-        .trim()
-        .isLength({ min: 3 })
-        .escape()
-        .withMessage('A title is required'),
-      check('message')
-        .trim()
-        .isLength({ min: 5 })
-        .escape()
-        .withMessage('A message is required'),
-    ],
-    (request, response) => {
+    validations,
+
+    async (request, response, next) => {
+      try {
+        const errors = validationResult(request);
+
+        if (!errors.isEmpty()) {
+          request.session.feedback = {
+            errors: errors.array(),
+          };
+          return response.redirect('/feedback');
+        }
+
+        const { name, email, title, message } = request.body;
+        await feedbackService.addEntry(name, email, title, message);
+        request.session.feedback = {
+          message: 'Thank you for your feedback!',
+        };
+        return response.redirect('/feedback');
+      } catch (err) {
+        return next(err);
+      }
+    }
+  );
+
+  router.post('/api', validations, async (request, response, next) => {
+    try {
       const errors = validationResult(request);
 
       if (!errors.isEmpty()) {
-        request.session.feedback = {
-          errors: errors.array(),
-        };
-        return response.redirect('/feedback');
+        return response.json({ errors: errors.array() });
       }
-      return response.send('Feedback form posted');
+      const { name, email, title, message } = request.body;
+      await feedbackService.addEntry(name, email, title, message);
+      const feedback = await feedbackService.getList();
+      return response.json({ feedback, successMessage: 'Thank you for your feedback!' });
+    } catch (err) {
+      return next(err);
     }
-  );
+  });
 
   return router;
 };
